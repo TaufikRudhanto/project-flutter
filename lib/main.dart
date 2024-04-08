2004b9d01a980434fe8c2ad916f0613696a70b5e
TaufikRudhanto <rudhanto22@gmail.com>
2024-04-08 12:52:22 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'first app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+          backgroundColor: Color(0xFF57A7D5),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Taufik Rudhanto',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20), // Spacer antara teks yang sudah ada dengan teks baru
+              Text(
+                'STI202102183',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color(0xFF211E1E),
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 50, 81, 193),
+      ),
+    );
+  }
+}
